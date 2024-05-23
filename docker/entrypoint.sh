8ead27c91e1643a7032d878985863cf744aaebc0
frnyb <ffn@sdu.dk>
2024-05-23 09:04:40 +0000

    Made containers work initially

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,9 +4,6 @@ set -e
 # Source the ROS2 setup script
 source /opt/ros/$ROS_DISTRO/setup.bash
 
-# Build the workspace
-cd /home/ws
-
 # Source the workspace setup script
 source install/setup.bash
 
