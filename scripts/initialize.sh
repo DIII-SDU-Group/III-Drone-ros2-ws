7859646b5475c020c535b692255b022552fa459f
frnyb <ffn@mmmi.sdu.dk>
2024-05-27 10:03:45 +0000

    Some changes to initialization scripts

--- scripts/initialize.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Create docker containers
-docker compose -f docker-compose.yml --profile "*" up --build --no-start
---
+++ scripts/post_start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Build workspace
+colcon build --cmake-args -DCMAKE_BUILD_TYPE=Debug
