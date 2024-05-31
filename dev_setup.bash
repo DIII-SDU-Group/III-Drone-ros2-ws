69dbec3cb19bf05d4f8c801e9448bee21274fc6e
frnyb <ffn@mmmi.sdu.dk>
2024-05-31 17:02:31 +0000

    Supervisor and CLI

--- dev_setup.bash
+++ dev_setup.bash
@@ -1,3 +1,4 @@
 export CONFIG_BASE_DIR="/home/iii/ws/.config"
 export SIMULATION="true"
 export SUPERVISOR_CONFIG_FILE="/home/iii/ws/src/III-Drone-Supervision/supervision_config/sim.yaml"
+export TMUXINATOR_PROJECT="iii_dev_launch"
