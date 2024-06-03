71e579d352aae38e5fa8fff920dc78ccaaf60411
frnyb <ffn@mmmi.sdu.dk>
2024-06-03 08:45:45 +0000

    Now properly installs the CLI

--- scripts/post_create.sh
+++ scripts/post_create.sh
@@ -4,9 +4,13 @@
 pip3 install -e ./tools/III-Drone-CLI
 
 # If eval "$(register-python-argcomplete iii)" is not in ~/.bashrc, add it
-if ! grep -q "eval \"\$(register-python-argcomplete iii)\"" ~/.bashrc; then
-    echo "eval \"\$(register-python-argcomplete iii)\"" >> ~/.bashrc
+if ! grep -q "eval \"\$(register-python-argcomplete3 iii)\"" ~/.bashrc; then
+    echo "eval \"\$(register-python-argcomplete3 iii)\"" >> ~/.bashrc
 fi
+
+# Reinstall the III-Drone-CLI
+pip3 uninstall -y iii 2> /dev/null
+pip3 install -e ./tools/III-Drone-CLI
 
 # If source /home/iii/ws/dev_setup.bash is not in ~/.bashrc, add it
 if ! grep -q "source /home/iii/ws/dev_setup.bash" ~/.bashrc; then
