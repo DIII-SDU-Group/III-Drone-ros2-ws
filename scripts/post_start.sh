1324c2b9373cff7aaead26272ecb7ce01c3a6f14
frnyb <ffn@mmmi.sdu.dk>
2024-06-04 11:33:57 +0000

    Moved cli install to post start script

--- scripts/post_start.sh
+++ scripts/post_start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# If eval "$(register-python-argcomplete iii)" is not in ~/.bashrc, add it
+if ! grep -q "eval \"\$(register-python-argcomplete3 iii)\"" ~/.bashrc; then
+    echo "eval \"\$(register-python-argcomplete3 iii)\"" >> ~/.bashrc
+fi
+
+# Reinstall the III-Drone-CLI
+pip3 uninstall -y iii 2> /dev/null
+pip3 install -e ./tools/III-Drone-CLI
+
 # Build workspace
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Debug
