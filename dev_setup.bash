270d63260aefb69281ead7b762278aec7eba388a
frnyb <ffn@mmmi.sdu.dk>
2024-06-04 17:38:57 +0200

    Developing

--- dev_setup.bash
+++ dev_setup.bash
@@ -3,4 +3,4 @@ export SIMULATION="true"
 export SUPERVISOR_CONFIG_FILE="/home/iii/ws/src/III-Drone-Supervision/supervision_config/sim.yaml"
 export TMUXINATOR_PROJECT="iii_dev_launch"
 
-export HOUGH_TRANSFORMER_LOG_LEVEL="debug"
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
