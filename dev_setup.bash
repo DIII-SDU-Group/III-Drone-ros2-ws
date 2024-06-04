77d3cfa87116f8b06908a77123b675ae986ef2fa
frnyb <ffn@mmmi.sdu.dk>
2024-06-04 11:33:15 +0000

    Added log level env variable

--- dev_setup.bash
+++ dev_setup.bash
@@ -2,3 +2,5 @@ export CONFIG_BASE_DIR="/home/iii/ws/.config"
 export SIMULATION="true"
 export SUPERVISOR_CONFIG_FILE="/home/iii/ws/src/III-Drone-Supervision/supervision_config/sim.yaml"
 export TMUXINATOR_PROJECT="iii_dev_launch"
+
+export HOUGH_TRANSFORMER_LOG_LEVEL="debug"
