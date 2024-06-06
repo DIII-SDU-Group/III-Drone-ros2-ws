63e8c2fd96c00a1c945efe915e5adab7594ff94f
frnyb <ffn@mmmi.sdu.dk>
2024-06-06 07:18:23 +0000

    Added log level environment variables

--- dev_setup.bash
+++ dev_setup.bash
@@ -4,3 +4,8 @@ export SUPERVISOR_CONFIG_FILE="/home/iii/ws/src/III-Drone-Supervision/supervisio
 export TMUXINATOR_PROJECT="iii_dev_launch"
 
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+
+export SUPERVISOR_LOG_LEVEL="info"
+export CONFIGURATION_SERVER_LOG_LEVEL="info"
+export DRONE_FRAME_BROADCASTER_LOG_LEVEL="info"
+export HOUGH_TRANSFORMER_LOG_LEVEL="debug"
