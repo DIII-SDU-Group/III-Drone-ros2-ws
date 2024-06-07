c0481a23965578b8d88671526708a2fc9ce52a83
frnyb <ffn@mmmi.sdu.dk>
2024-06-07 16:34:33 +0200

    Developing

---
+++ real_setup.bash
@@ -0,0 +1,15 @@
+export CONFIG_BASE_DIR="/home/iii/ws/.config"
+export SIMULATION="true"
+export SUPERVISOR_CONFIG_FILE="/home/iii/ws/src/III-Drone-Supervision/supervision_config/sim.yaml"
+export TMUXINATOR_PROJECT="iii_dev_launch"
+
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+
+export SUPERVISOR_LOG_LEVEL="info"
+export CONFIGURATION_SERVER_LOG_LEVEL="info"
+export DRONE_FRAME_BROADCASTER_LOG_LEVEL="info"
+export HOUGH_TRANSFORMER_LOG_LEVEL="info"
+export PL_DIR_COMPUTER_LOG_LEVEL="info"
+export PL_MAPPER_LOG_LEVEL="info"
+export TRAJECTORY_GENERATOR_LOG_LEVEL="info"
+export MANEUVER_CONTROLLER_LOG_LEVEL="debug"
