d6401e2aa8f7e7477d28b5395d3ae069769f2e91
frnyb <ffn@mmmi.sdu.dk>
2024-06-14 15:06:35 +0000

    Contain paths in separate file

--- setup/setup_dev.bash
+++ setup/setup_dev.bash
@@ -1,14 +1,15 @@
 export CLI_CONFIGURATION="dev"
 
-export CONFIG_BASE_DIR="/home/iii/ws/.config"
-export SIMULATION="true"
-export SUPERVISOR_CONFIG_FILE="/home/iii/ws/src/III-Drone-Supervision/supervision_config/sim.yaml"
-export TMUXINATOR_PROJECT="iii_dev_launch"
-
-export COLCON_HOME="/home/iii/ws"
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export WORKSPACE_DIR="$(dirname $SCRIPT_DIR)"
+
+source $SCRIPT_DIR/paths.bash
+
+export SIMULATION="true"
+export SUPERVISOR_CONFIG_FILE="$SUPERVISOR_CONFIG_DIR/sim.yaml"
+export TMUXINATOR_PROJECT="iii_dev_launch"
+
+export COLCON_HOME="$WORKSPACE_DIR"
 
 source $SCRIPT_DIR/remote.bash
 source $SCRIPT_DIR/node_log_levels.bash
--- setup/setup_real.bash
+++ setup/setup_real.bash
@@ -1,15 +1,16 @@
 source /arm64-sysroot/opt/ros/humble/setup.bash
 source /arm64-sysroot/home/iii/ws/install/setup.bash 2> /dev/null
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $SCRIPT_DIR/paths.bash
+
 export CLI_CONFIGURATION="container"
 
-export CONFIG_BASE_DIR="/home/iii/ws/.config"
 export TMUXINATOR_PROJECT="iii_real_launch"
 
 export SIMULATION="false"
-export SUPERVISOR_CONFIG_FILE="/home/iii/ws/src/III-Drone-Supervision/supervision_config/real.yaml"
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export SUPERVISOR_CONFIG_FILE="$SUPERVISOR_CONFIG_DIR/real.yaml"
 
 source $SCRIPT_DIR/node_log_levels.bash
 source $SCRIPT_DIR/ros_setup.bash
